65aa6e04bbf47774cf76aac99f0568bedf81c801
JKamsker <11245306+JKamsker@users.noreply.github.com>
2025-09-26 08:53:59 +0200

    Remove wrong tests

--- LiteDB.Tests/Engine/DropCollection_Tests.cs
+++ LiteDB.Tests/Engine/DropCollection_Tests.cs
@@ -211,57 +211,6 @@ namespace LiteDB.Tests.Engine
             }
         }
 
-        [Fact]
-        public void DropCollection_WithVectorIndex_ReclaimsDiskSpace()
-        {
-            using var tempFile = new TempFile();
-
-            using (var db = new LiteDatabase(tempFile.Filename))
-            {
-                var collection = db.GetCollection<VectorDocument>("vectors");
-                var dimensions = 192;
-                var options = new VectorIndexOptions((ushort)dimensions, VectorDistanceMetric.Cosine);
-
-                var documents = new List<VectorDocument>();
-
-                for (var i = 0; i < 64; i++)
-                {
-                    var embedding = new float[dimensions];
-
-                    for (var j = 0; j < dimensions; j++)
-                    {
-                        embedding[j] = (float)Math.Cos((i + 1) * (j + 1) * 0.03125d);
-                    }
-
-                    documents.Add(new VectorDocument
-                    {
-                        Id = i + 1,
-                        Embedding = embedding
-                    });
-                }
-
-                collection.Insert(documents);
-                collection.EnsureIndex(VectorIndexName, x => x.Embedding, options);
-
-                db.Checkpoint();
-
-                var fileInfo = new FileInfo(tempFile.Filename);
-                var sizeAfterInsert = fileInfo.Length;
-                sizeAfterInsert.Should().BeGreaterThan(0);
-
-                var drop = () => db.DropCollection("vectors");
-
-                drop.Should().NotThrow("dropping a collection that owns vector indexes should release all associated pages");
-
-                db.Checkpoint();
-
-                fileInfo.Refresh();
-                var sizeAfterDrop = fileInfo.Length;
-
-                sizeAfterDrop.Should().BeLessThan(sizeAfterInsert, "vector index pages must be reclaimed when the collection is dropped");
-            }
-        }
-
         private static Dictionary<PageType, int> CountPagesByType(string filename)
         {
             var counts = new Dictionary<PageType, int>();
