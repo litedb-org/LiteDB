95c943fc3b297fff56f9cab71944aeb44e5534bd
JKamsker <11245306+JKamsker@users.noreply.github.com>
2025-10-06 10:10:41 +0200

    Re-introduce normalization in sha1 hash

--- LiteDB/Client/Shared/SharedMutexNameFactory.cs
+++ LiteDB/Client/Shared/SharedMutexNameFactory.cs
@@ -12,27 +12,32 @@ internal static class SharedMutexNameFactory
     {
         return strategy switch
         {
-            SharedMutexNameStrategy.UriEscape or SharedMutexNameStrategy.Default  => Uri.EscapeDataString(Path.GetFullPath(fileName).ToLowerInvariant()),
-            SharedMutexNameStrategy.Sha1Hash => Sha1(fileName),
+            SharedMutexNameStrategy.UriEscape or SharedMutexNameStrategy.Default => CreateUsingUriEncoding(fileName),
+            SharedMutexNameStrategy.Sha1Hash => CreateUsingSha1(fileName),
             _ => throw new ArgumentOutOfRangeException(nameof(strategy), strategy, null)
         };
     }
-    
-    internal static string Sha1(string value)
+
+    private static string CreateUsingUriEncoding(string fileName)
     {
-        var data = Encoding.UTF8.GetBytes(value);
+        var normalized = Path.GetFullPath(fileName).ToLowerInvariant();
+        return Uri.EscapeDataString(normalized);
+    }
 
-        using (var sha = SHA1.Create())
+    internal static string CreateUsingSha1(string value)
+    {
+        var normalized = Path.GetFullPath(value).ToLower();
+        var data = Encoding.UTF8.GetBytes(normalized);
+        
+        using var sha = SHA1.Create();
+        var hashData = sha.ComputeHash(data);
+        var hash = new StringBuilder();
+
+        foreach (var b in hashData)
         {
-            var hashData = sha.ComputeHash(data);
-            var hash = new StringBuilder();
-
-            foreach (var b in hashData)
-            {
-                hash.Append(b.ToString("X2"));
-            }
-
-            return hash.ToString();
+            hash.Append(b.ToString("X2"));
         }
+
+        return hash.ToString();
     }
 }
