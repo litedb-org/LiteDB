97ed8414cd0ea3e7bf04ccb84e6187aec01796c3
TyKonKet <matteoautieri@live.it>
2025-11-16 16:46:33 +0100

    Refactored XML documentation for clarity and consistency, adding detailed `<param>`, `<returns>`, and `<exception>` tags.

--- LiteDB/Client/Structures/Query.cs
+++ LiteDB/Client/Structures/Query.cs
@@ -8,32 +8,32 @@ using static LiteDB.Constants;
 
 namespace LiteDB
 {
-    /// <summary>
-    /// Class is a result from optimized QueryBuild. Indicate how engine must run query - there is no more decisions to engine made, must only execute as query was defined
-    /// </summary>
     public partial class Query
     {
         /// <summary>
-        /// Indicate when a query must execute in ascending order
+        /// Constant indicating ascending sort order (1).
         /// </summary>
         public const int Ascending = 1;
 
         /// <summary>
-        /// Indicate when a query must execute in descending order
+        /// Constant indicating descending sort order (-1).
         /// </summary>
         public const int Descending = -1;
 
         /// <summary>
-        /// Returns all documents
+        /// Creates a query that returns all documents in the collection without ordering.
         /// </summary>
+        /// <returns>A <see cref="Query"/> instance that selects all documents.</returns>
         public static Query All()
         {
             return new Query();
         }
 
         /// <summary>
-        /// Returns all documents
+        /// Creates a query that returns all documents ordered by the <c>_id</c> field.
         /// </summary>
+        /// <param name="order">The sort order: <see cref="Ascending"/> (1) or <see cref="Descending"/> (-1). Default is <see cref="Ascending"/>.</param>
+        /// <returns>A <see cref="Query"/> instance that selects all documents ordered by <c>_id</c>.</returns>
         public static Query All(int order = Ascending)
         {
             var query = new Query();
@@ -42,8 +42,11 @@ namespace LiteDB
         }
 
         /// <summary>
-        /// Returns all documents
+        /// Creates a query that returns all documents ordered by the specified field.
         /// </summary>
+        /// <param name="field">The field name to order by.</param>
+        /// <param name="order">The sort order: <see cref="Ascending"/> (1) or <see cref="Descending"/> (-1). Default is <see cref="Ascending"/>.</param>
+        /// <returns>A <see cref="Query"/> instance that selects all documents ordered by the specified field.</returns>
         public static Query All(string field, int order = Ascending)
         {
             var query = new Query();
@@ -52,8 +55,12 @@ namespace LiteDB
         }
 
         /// <summary>
-        /// Returns all documents that value are equals to value (=)
+        /// Creates an expression that matches documents where the field value equals the specified value.
         /// </summary>
+        /// <param name="field">The field name to compare.</param>
+        /// <param name="value">The value to match.</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the equality comparison (=).</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="field"/> is <see langword="null"/> or whitespace.</exception>
         public static BsonExpression EQ(string field, BsonValue value)
         {
             if (field.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(field));
@@ -62,8 +69,12 @@ namespace LiteDB
         }
 
         /// <summary>
-        /// Returns all documents that value are less than value (&lt;)
+        /// Creates an expression that matches documents where the field value is less than the specified value.
         /// </summary>
+        /// <param name="field">The field name to compare.</param>
+        /// <param name="value">The value to compare against.</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the less-than comparison (&lt;).</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="field"/> is <see langword="null"/> or whitespace.</exception>
         public static BsonExpression LT(string field, BsonValue value)
         {
             if (field.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(field));
@@ -72,8 +83,12 @@ namespace LiteDB
         }
 
         /// <summary>
-        /// Returns all documents that value are less than or equals value (&lt;=)
+        /// Creates an expression that matches documents where the field value is less than or equal to the specified value.
         /// </summary>
+        /// <param name="field">The field name to compare.</param>
+        /// <param name="value">The value to compare against.</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the less-than-or-equal comparison (&lt;=).</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="field"/> is <see langword="null"/> or whitespace.</exception>
         public static BsonExpression LTE(string field, BsonValue value)
         {
             if (field.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(field));
@@ -82,8 +97,12 @@ namespace LiteDB
         }
 
         /// <summary>
-        /// Returns all document that value are greater than value (&gt;)
+        /// Creates an expression that matches documents where the field value is greater than the specified value.
         /// </summary>
+        /// <param name="field">The field name to compare.</param>
+        /// <param name="value">The value to compare against.</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the greater-than comparison (&gt;).</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="field"/> is <see langword="null"/> or whitespace.</exception>
         public static BsonExpression GT(string field, BsonValue value)
         {
             if (field.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(field));
@@ -92,8 +111,12 @@ namespace LiteDB
         }
 
         /// <summary>
-        /// Returns all documents that value are greater than or equals value (&gt;=)
+        /// Creates an expression that matches documents where the field value is greater than or equal to the specified value.
         /// </summary>
+        /// <param name="field">The field name to compare.</param>
+        /// <param name="value">The value to compare against.</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the greater-than-or-equal comparison (&gt;=).</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="field"/> is <see langword="null"/> or whitespace.</exception>
         public static BsonExpression GTE(string field, BsonValue value)
         {
             if (field.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(field));
@@ -102,8 +125,13 @@ namespace LiteDB
         }
 
         /// <summary>
-        /// Returns all document that values are between "start" and "end" values (BETWEEN)
+        /// Creates an expression that matches documents where the field value is between the start and end values (inclusive).
         /// </summary>
+        /// <param name="field">The field name to compare.</param>
+        /// <param name="start">The start value of the range (inclusive).</param>
+        /// <param name="end">The end value of the range (inclusive).</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the BETWEEN comparison.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="field"/> is <see langword="null"/> or whitespace.</exception>
         public static BsonExpression Between(string field, BsonValue start, BsonValue end)
         {
             if (field.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(field));
@@ -112,8 +140,12 @@ namespace LiteDB
         }
 
         /// <summary>
-        /// Returns all documents that starts with value (LIKE)
+        /// Creates an expression that matches documents where the string field value starts with the specified prefix.
         /// </summary>
+        /// <param name="field">The field name containing the string to check.</param>
+        /// <param name="value">The prefix string to match.</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the LIKE comparison with wildcard suffix.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="field"/> or <paramref name="value"/> is <see langword="null"/> or whitespace.</exception>
         public static BsonExpression StartsWith(string field, string value)
         {
             if (field.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(field));
@@ -123,8 +155,12 @@ namespace LiteDB
         }
 
         /// <summary>
-        /// Returns all documents that contains value (CONTAINS) - string Contains
+        /// Creates an expression that matches documents where the string field value contains the specified substring.
         /// </summary>
+        /// <param name="field">The field name containing the string to search.</param>
+        /// <param name="value">The substring to find.</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the LIKE comparison with wildcard prefix and suffix.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="field"/> or <paramref name="value"/> is <see langword="null"/> or empty.</exception>
         public static BsonExpression Contains(string field, string value)
         {
             if (field.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(field));
@@ -134,8 +170,12 @@ namespace LiteDB
         }
 
         /// <summary>
-        /// Returns all documents that are not equals to value (not equals)
+        /// Creates an expression that matches documents where the field value is not equal to the specified value.
         /// </summary>
+        /// <param name="field">The field name to compare.</param>
+        /// <param name="value">The value to exclude.</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the not-equal comparison (!=).</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="field"/> is <see langword="null"/> or whitespace.</exception>
         public static BsonExpression Not(string field, BsonValue value)
         {
             if (field.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(field));
@@ -144,8 +184,12 @@ namespace LiteDB
         }
 
         /// <summary>
-        /// Returns all documents that has value in values list (IN)
+        /// Creates an expression that matches documents where the field value is in the specified array of values.
         /// </summary>
+        /// <param name="field">The field name to check.</param>
+        /// <param name="value">The array of values to match against.</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the IN comparison.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="field"/> is <see langword="null"/> or whitespace, or when <paramref name="value"/> is <see langword="null"/>.</exception>
         public static BsonExpression In(string field, BsonArray value)
         {
             if (field.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(field));
@@ -155,29 +199,45 @@ namespace LiteDB
         }
 
         /// <summary>
-        /// Returns all documents that has value in values list (IN)
+        /// Creates an expression that matches documents where the field value is in the specified array of values.
         /// </summary>
+        /// <param name="field">The field name to check.</param>
+        /// <param name="values">The array of values to match against.</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the IN comparison.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="field"/> is <see langword="null"/> or whitespace, or when <paramref name="values"/> is <see langword="null"/>.</exception>
         public static BsonExpression In(string field, params BsonValue[] values)
         {
             return In(field, new BsonArray(values));
         }
 
         /// <summary>
-        /// Returns all documents that has value in values list (IN)
+        /// Creates an expression that matches documents where the field value is in the specified collection of values.
         /// </summary>
+        /// <param name="field">The field name to check.</param>
+        /// <param name="values">The collection of values to match against.</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the IN comparison.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="field"/> is null or whitespace.</exception>
         public static BsonExpression In(string field, IEnumerable<BsonValue> values)
         {
             return In(field, new BsonArray(values));
         }
 
         /// <summary>
-        /// Get all operands to works with array or enumerable values
+        /// Gets a <see cref="QueryAny"/> instance that provides methods for working with array or enumerable field values.
         /// </summary>
+        /// <returns>A <see cref="QueryAny"/> instance for array operations.</returns>
         public static QueryAny Any() => new QueryAny();
 
         /// <summary>
-        /// Returns document that exists in BOTH queries results. If both queries has indexes, left query has index preference (other side will be run in full scan)
+        /// Combines two expressions with logical AND, matching documents that satisfy both conditions.
         /// </summary>
+        /// <param name="left">The left expression.</param>
+        /// <param name="right">The right expression.</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the AND combination.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="left"/> or <paramref name="right"/> is <see langword="null"/>.</exception>
+        /// <remarks>
+        /// If both expressions use indexes, the left expression's index is preferred and the right side may perform a full scan.
+        /// </remarks>
         public static BsonExpression And(BsonExpression left, BsonExpression right)
         {
             if (left == null) throw new ArgumentNullException(nameof(left));
@@ -187,8 +247,11 @@ namespace LiteDB
         }
 
         /// <summary>
-        /// Returns document that exists in ALL queries results.
+        /// Combines multiple expressions with logical AND, matching documents that satisfy all conditions.
         /// </summary>
+        /// <param name="queries">The array of expressions to combine. At least two expressions are required.</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the AND combination of all expressions.</returns>
+        /// <exception cref="ArgumentException">Thrown when fewer than two expressions are provided.</exception>
         public static BsonExpression And(params BsonExpression[] queries)
         {
             if (queries == null || queries.Length < 2) throw new ArgumentException("At least two Query should be passed");
@@ -204,8 +267,12 @@ namespace LiteDB
         }
 
         /// <summary>
-        /// Returns documents that exists in ANY queries results (Union).
+        /// Combines two expressions with logical OR, matching documents that satisfy either condition (union).
         /// </summary>
+        /// <param name="left">The left expression.</param>
+        /// <param name="right">The right expression.</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the OR combination.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="left"/> or <paramref name="right"/> is <see langword="null"/>.</exception>
         public static BsonExpression Or(BsonExpression left, BsonExpression right)
         {
             if (left == null) throw new ArgumentNullException(nameof(left));
@@ -215,8 +282,11 @@ namespace LiteDB
         }
 
         /// <summary>
-        /// Returns document that exists in ANY queries results (Union).
+        /// Combines multiple expressions with logical OR, matching documents that satisfy any condition (union).
         /// </summary>
+        /// <param name="queries">The array of expressions to combine. At least two expressions are required.</param>
+        /// <returns>A <see cref="BsonExpression"/> representing the OR combination of all expressions.</returns>
+        /// <exception cref="ArgumentException">Thrown when fewer than two expressions are provided.</exception>
         public static BsonExpression Or(params BsonExpression[] queries)
         {
             if (queries == null || queries.Length < 2) throw new ArgumentException("At least two Query should be passed");
--- LiteDB/Engine/Query/Query.cs
+++ LiteDB/Engine/Query/Query.cs
@@ -8,35 +8,102 @@ using static LiteDB.Constants;
 namespace LiteDB
 {
     /// <summary>
-    /// Represent full query options
+    /// Represents the complete structure of a query including filtering, ordering, grouping, and projection options.
     /// </summary>
+    /// <remarks>
+    /// This class is typically built using the fluent interface provided by <see cref="ILiteQueryable{T}"/> methods.
+    /// It contains all query components that will be executed against a LiteDB collection.
+    /// </remarks>
     public partial class Query
     {
+        /// <summary>
+        /// Gets or sets the SELECT expression that defines the projection of query results. Default is <see cref="BsonExpression.Root"/> (all fields).
+        /// </summary>
         public BsonExpression Select { get; set; } = BsonExpression.Root;
 
+        /// <summary>
+        /// Gets the list of INCLUDE expressions for loading related documents via DbRef.
+        /// </summary>
         public List<BsonExpression> Includes { get; } = new List<BsonExpression>();
+
+        /// <summary>
+        /// Gets the list of WHERE expressions that filter documents. Multiple expressions are combined with AND logic.
+        /// </summary>
         public List<BsonExpression> Where { get; } = new List<BsonExpression>();
 
+        /// <summary>
+        /// Gets the list of ORDER BY expressions that define the sort order of results.
+        /// </summary>
         public List<QueryOrder> OrderBy { get; } = new List<QueryOrder>();
 
+        /// <summary>
+        /// Gets or sets the GROUP BY expression for grouping results. Default is <see langword="null"/> (no grouping).
+        /// </summary>
         public BsonExpression GroupBy { get; set; } = null;
+
+        /// <summary>
+        /// Gets or sets the HAVING expression that filters grouped results. Default is <see langword="null"/> (no filter).
+        /// </summary>
         public BsonExpression Having { get; set; } = null;
 
+        /// <summary>
+        /// Gets or sets the number of documents to skip in the result set. Default is 0.
+        /// </summary>
         public int Offset { get; set; } = 0;
+
+        /// <summary>
+        /// Gets or sets the maximum number of documents to return. Default is <see cref="int.MaxValue"/> (no limit).
+        /// </summary>
         public int Limit { get; set; } = int.MaxValue;
+
+        /// <summary>
+        /// Gets or sets whether to execute the query with a write lock for update operations. Default is <see langword="false"/>.
+        /// </summary>
         public bool ForUpdate { get; set; } = false;
 
+        /// <summary>
+        /// Gets or sets the field name for vector similarity search. Default is <see langword="null"/>.
+        /// </summary>
         public string VectorField { get; set; } = null;
+
+        /// <summary>
+        /// Gets or sets the target vector for similarity search. Default is <see langword="null"/>.
+        /// </summary>
         public float[] VectorTarget { get; set; } = null;
+
+        /// <summary>
+        /// Gets or sets the maximum distance threshold for vector similarity search. Default is <see cref="double.MaxValue"/> (no limit).
+        /// </summary>
         public double VectorMaxDistance { get; set; } = double.MaxValue;
+
+        /// <summary>
+        /// Gets whether this query has a vector similarity filter configured.
+        /// </summary>
         public bool HasVectorFilter => VectorField != null && VectorTarget != null;
 
+        /// <summary>
+        /// Gets or sets the name of the collection where query results will be inserted (SELECT INTO). Default is <see langword="null"/>.
+        /// </summary>
         public string Into { get; set; }
+
+        /// <summary>
+        /// Gets or sets the auto-ID generation strategy for the INTO collection. Default is <see cref="BsonAutoId.ObjectId"/>.
+        /// </summary>
         public BsonAutoId IntoAutoId { get; set; } = BsonAutoId.ObjectId;
 
+        /// <summary>
+        /// Gets or sets whether to return the query execution plan instead of executing the query. Default is <see langword="false"/>.
+        /// </summary>
         public bool ExplainPlan { get; set; }
 
         /// <summary>
+        /// Converts this query structure to a SQL-like string representation.
+        /// </summary>
+        /// <param name="collection">The collection name to include in the SQL statement.</param>
+        /// <returns>A SQL-like string representing the complete query structure.</returns>
+        /// <remarks>
+        /// <para>The SQL format follows this structure:</para>
+        /// <code>
         /// [ EXPLAIN ]
         ///    SELECT {selectExpr}
         ///    [ INTO {newcollection|$function} [ : {autoId} ] ]
@@ -49,7 +116,8 @@ namespace LiteDB
         ///   [ LIMIT {number} ]
         ///  [ OFFSET {number} ]
         ///     [ FOR UPDATE ]
-        /// </summary>
+        /// </code>
+        /// </remarks>
         public string ToSQL(string collection)
         {
             var sb = new StringBuilder();
