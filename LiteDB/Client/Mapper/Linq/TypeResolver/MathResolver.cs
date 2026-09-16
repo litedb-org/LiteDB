c15570d702a80e45c2f962e5d624bbeb44858e45
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-09-16 11:26:12 +0000

    Fix #2779: evaluate closed calls unsupported by query resolvers

--- LiteDB/Client/Mapper/Linq/TypeResolver/MathResolver.cs
+++ LiteDB/Client/Mapper/Linq/TypeResolver/MathResolver.cs
@@ -20,7 +20,7 @@ namespace LiteDB
                 case "Abs": return "ABS(@0)";
                 case "Pow": return "POW(@0, @1)";
                 case "Round":
-                    if (qtParams != 2) throw new ArgumentOutOfRangeException("Method Round need 2 arguments when convert to BsonExpression");
+                    if (qtParams != 2 || method.GetParameters()[1].ParameterType != typeof(int)) return null;
                     return "ROUND(@0, @1)";
             }
 
