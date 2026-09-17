f8299f43f0327d584705fe272232b4f2f7a49742
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-09-17 11:49:46 +0000

    Fix #2864 include actual page types in validation errors

--- LiteDB/Engine/Pages/IndexPage.cs
+++ LiteDB/Engine/Pages/IndexPage.cs
@@ -16,7 +16,10 @@ namespace LiteDB.Engine
         public IndexPage(PageBuffer buffer)
             : base(buffer)
         {
-            ENSURE(this.PageType == PageType.Index, "page type must be index page");
+            if (this.PageType != PageType.Index)
+            {
+                ENSURE(false, "page type must be index page, but it is {0}", this.PageType);
+            }
 
             if (this.PageType != PageType.Index) throw LiteException.InvalidPageType(PageType.Index, this);
         }
